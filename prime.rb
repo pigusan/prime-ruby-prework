7eddd84c6ba540e6a3cfc20d5c7022dc4c69e5ac
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 17:41:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,10 @@ def prime?(number)
   array = (1..number).to_a
   number_of_factors = 0
   array.each do |numbers|
-    checking_if_factor = number.to_f/numbers
-    if checking_if_factor.is_a? Integer
-      number_of_factors += 1
+    rounded_division_check = number/numbers
+    non_rounded_division_check = number.to_f/numbers
+    if non_rounded_division_check == rounded_division_check
+        number_of_factors += 1
     end
   end
   if number_of_factors == 2
@@ -13,7 +14,6 @@ def prime?(number)
     false
   end
 end
-
 #given a number
 
 #create a range from 1 to that number
