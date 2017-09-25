4380d198ecf4aeae2cac413984c08db7216c30c8
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 16:16:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-# Add  code here!
 def prime?(number)
+  
 end
 
+#given a number
+
+#create a range from 1 to that number
+#turn that range into an array
+#keep track of # of factors
+#iterate over that array seeing if number is can be divided by array 
+#if # of factors is greater than 2 false
+#else true
